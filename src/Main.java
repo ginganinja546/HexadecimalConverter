6ed2d27d024a51c8416196a71ac160abfeab60d4
trist <tlsorens@mtu.edu>
2021-11-09 00:08:26 -0500

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,24 @@ public class Main {
     // A12 = (10 * 16^2) + (1 * 16^1) + (2 * 16^0)
 
     public static void main(String[] args) {
-        //System.out.println(HexToDecimal.hexToDecimal("A12"));
+        int a = HexToDecimal.hexToDecimal("A12");
+        System.out.println(a);
 
-        //System.out.println(DecimalToHex.decimalToHexDigit(1));
-        System.out.println(DecimalToHex.decimalToHex(514252));
+        System.out.println(DecimalToHex.decimalToHex(a));
     }
 
-    class HexToDecimal {
+    static class HexToDecimal {
 
         private static int hexToDecimalDigit(char c) {
-            switch (c) {
-                case 'A':
-                    return 10;
-                case 'B':
-                    return 11;
-                case 'C':
-                    return 12;
-                case 'D':
-                    return 13;
-                case 'E':
-                    return 14;
-                case 'F':
-                    return 15;
-                default:
-                    return Integer.parseInt(Character.toString(c));
-            }
+            return switch (c) {
+                case 'A' -> 10;
+                case 'B' -> 11;
+                case 'C' -> 12;
+                case 'D' -> 13;
+                case 'E' -> 14;
+                case 'F' -> 15;
+                default -> Integer.parseInt(Character.toString(c));
+            };
         }
 
         private static int[] hexStringToDigits(String hex_num) {
@@ -63,7 +56,7 @@ public class Main {
 
     }
 
-    class DecimalToHex {
+    static class DecimalToHex {
         // Divide by 16 and take remainder continuously until remainder is 0
         // 2578 / 16 = 161 R 2 --> 161 / 16 = 160 R 1 --> 10 / 16 = 0 R 10 => 2, 1, A = A12
 
@@ -72,27 +65,19 @@ public class Main {
                 System.err.println("Hex digit must be constrained by 0 - 15");
                 return null; // null character (not 0)
             }
-            switch(digit) {
-                case 0:
-                    return "0";
-                case 10:
-                    return "A";
-                case 11:
-                    return "B";
-                case 12:
-                    return "C";
-                case 13:
-                    return "D";
-                case 14:
-                    return "E";
-                case 15:
-                    return "F";
-                default:
-                    return Integer.toString(digit);
-            }
+            return switch (digit) {
+                case 0 -> "0";
+                case 10 -> "A";
+                case 11 -> "B";
+                case 12 -> "C";
+                case 13 -> "D";
+                case 14 -> "E";
+                case 15 -> "F";
+                default -> Integer.toString(digit);
+            };
         }
 
-        private static ArrayList<Integer> remainders = new ArrayList<>();
+        private static final ArrayList<Integer> remainders = new ArrayList<>();
         private static ArrayList<Integer> getHexDigits(int dec_num) {
             if (dec_num == 0) return remainders;
 
@@ -103,12 +88,12 @@ public class Main {
         }
 
         public static String decimalToHex(int dec_num) {
-            String hex = "";
+            StringBuilder hex = new StringBuilder();
             getHexDigits(dec_num);
             for (int i = remainders.size()-1; i >= 0; i--) {
-                hex += decimalToHexDigit(remainders.get(i));
+                hex.append(decimalToHexDigit(remainders.get(i)));
             }
-            return hex;
+            return hex.toString();
         }
 
     }
